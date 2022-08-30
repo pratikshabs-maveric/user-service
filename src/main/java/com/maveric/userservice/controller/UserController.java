2215271865043fdf242b38f0fecbc242c8b66380
pratikshabs-maveric <pratikshab@maveric-systems.com>
2022-08-30 12:08:01 +0530

    Deleted unwanted code

--- src/main/java/com/maveric/userservice/controller/UserController.java
+++ src/main/java/com/maveric/userservice/controller/UserController.java
@@ -14,71 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 
-//@RequestMapping("/api/v1/users")
-//@RestController
-//public class UserController {
-//
-//    @Autowired
-//    private UserRepository userRepository;
-//
-//    // find all users
-//    @GetMapping
-//    public List<User> getUserDetails(){
-//    return userRepository.findAll();
-//    }
-//
-//    // create user
-//    @PostMapping // creating resource
-//    public User createUser( @RequestBody User user){
-//        return userRepository.save(user);
-//    }
-//
-//    //get user by id
-//    @GetMapping("{userId}")
-//    public ResponseEntity<User> getUserDetails( @PathVariable long userId){
-//        User user = userRepository.findById(userId).orElseThrow(()-> new UserNotFound("User Not Found"));
-//        return ResponseEntity.ok(user);
-//    }
-//
-//    // get by email id
-//    @GetMapping("/getUserByEmail/{email}")
-//    public ResponseEntity<User> getUserDetailsByEmail(@PathVariable("email") String email){
-//        User userByEmail = userRepository.findByEmail(email);
-//        if(userByEmail==null){
-//            throw new EmailNotFound("Email not Found");
-//        }
-//        return ResponseEntity.ok(userByEmail);
-//    }
-//
-//    //update user rest api
-//    @PutMapping("{userId}") // update the resource
-//    public ResponseEntity<User> updateUser( @PathVariable long userId, @RequestBody User userDetails){
-//        User updateUser = userRepository.findById(userId).orElseThrow(()-> new UserNotFound("User Not Found"));
-//
-//        updateUser.setFirstName(userDetails.getFirstName());
-//        updateUser.setLastName(userDetails.getLastName());
-//        updateUser.setMiddleName(userDetails.getMiddleName());
-//        updateUser.setPhoneNumber(userDetails.getPhoneNumber());
-//        updateUser.setEmail(userDetails.getEmail());
-//        updateUser.setAddress(userDetails.getAddress());
-//        updateUser.setDateOfBirth(userDetails.getDateOfBirth());
-//        updateUser.setGender(userDetails.getGender());
-//        userRepository.save(updateUser);
-//
-//        return ResponseEntity.ok(updateUser);
-//    }
-//
-//    //delete user REST API
-//    @DeleteMapping("{userId}")
-//    public ResponseEntity<HttpStatus> deleteUser( @PathVariable long userId) {
-//        User deleteUser = userRepository.findById(userId).orElseThrow(()-> new UserNotFound("User Not Found"));
-//        userRepository.delete(deleteUser);
-//
-//        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-//    }
-//
-//
-//}
+
 @RequestMapping("/api/v1")
 @RestController
 public class UserController {
