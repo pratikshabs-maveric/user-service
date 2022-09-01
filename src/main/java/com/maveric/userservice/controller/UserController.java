df59ee7b93940aa1bc1c6321fcf3040fecbcc83c
pratikshabs-maveric <pratikshab@maveric-systems.com>
2022-09-01 09:46:29 +0530

    Moved code in feature branch

--- src/main/java/com/maveric/userservice/controller/UserController.java
+++ src/main/java/com/maveric/userservice/controller/UserController.java
@@ -1,58 +1,5 @@
 package com.maveric.userservice.controller;
 
-import com.maveric.userservice.dto.UserResponse;
-import com.maveric.userservice.model.User;
-import com.maveric.userservice.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-
-@RequestMapping("/api/v1/users")
-@RestController
 public class UserController {
 
-    @Autowired
-    UserService userService;
-
-    // Get the list of users
-    public ResponseEntity<List<UserResponse>> getUsers(@RequestParam(defaultValue = "0") Integer page, @RequestParam(defaultValue = "10") Integer pageSize){
-        List<UserResponse> userResponses = userService.getUsers(page,pageSize);
-        return new ResponseEntity<List<UserResponse>>(userResponses, HttpStatus.OK);
-    }
-
-    // Create user using REST API
-    public ResponseEntity<UserResponse> createUser(@RequestBody UserResponse userResponse) {
-        UserResponse userDtoResponse = userService.createUser(userResponse);
-        return new ResponseEntity<UserResponse>(userDtoResponse, HttpStatus.OK);
-    }
-
-   // Get users details
-    @GetMapping("{userId}")
-    public ResponseEntity<UserResponse> getUserDetails(@PathVariable String userId) {
-        UserResponse userDtoResponse = userService.getUserDetails(userId);
-        return new ResponseEntity<UserResponse>(userDtoResponse, HttpStatus.OK);
-    }
-
-    // Update user
-    @PutMapping("{userId}")
-    public ResponseEntity<UserResponse> updateUser(@PathVariable long userId, @RequestBody User User){
-        return new ResponseEntity<UserResponse>(userService.updateUser(userId, User), HttpStatus.OK);
-    }
-
-    // Delete user
-    @DeleteMapping("{userId}")
-    public ResponseEntity<String> deleteUser(@PathVariable String userId) {
-        String result = userService.deleteUser(userId);
-        return new ResponseEntity<String>(result, HttpStatus.OK);
-    }
-
-    // Get user by email ID
-    @GetMapping("getUserByEmail/{email}")
-    public ResponseEntity<UserResponse> getUserDetailsByEmail(@PathVariable String email) {
-        UserResponse res = userService.getUserDetailsByEmail(email);
-        return new ResponseEntity<UserResponse>(res, HttpStatus.OK);
-    }
 }
--- src/main/java/com/maveric/userservice/service/UserService.java
+++ src/main/java/com/maveric/userservice/service/UserService.java
@@ -1,22 +1,5 @@
 package com.maveric.userservice.service;
 
-import com.maveric.userservice.dto.UserResponse;
-import com.maveric.userservice.exceptionhandler.UserNotExist;
-import com.maveric.userservice.model.User;
-
-import java.util.List;
-
 public interface UserService {
-    public List<UserResponse> getUsers(Integer page, Integer pageSize);
-    public UserResponse createUser(UserResponse userResponse);
-
-    public UserResponse getUserDetails(String userId);
-
-    public String deleteUser(String userId);
-
-    public UserResponse  getUserDetailsByEmail(String email);
-
-    public UserResponse updateUser(long userId, User user) throws UserNotExist;
-
 
 }
